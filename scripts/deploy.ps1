9c7521304c3f7fe217f6fb9d10c4058e1c0a31f6
Matt Mazzola <mattmazzola@live.com>
2023-08-25 20:16:39 -0700

    Upgrade deployment script

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,7 +1,4 @@
-$sharedResourceGroupName = "shared"
-$sharedRgString = 'klgoyi'
-$resourceGroupLocation = "westus3"
-$personalProjectsResourceGroupName = "personalprojects"
+Param([switch]$WhatIf = $True)
 
 echo "PScriptRoot: $PScriptRoot"
 $repoRoot = If ('' -eq $PScriptRoot) {
@@ -15,8 +12,16 @@ echo "Repo Root: $repoRoot"
 
 Import-Module "C:/repos/shared-resources/pipelines/scripts/common.psm1" -Force
 
-Write-Step "Create Resource Group"
-az group create -l $resourceGroupLocation -g $personalProjectsResourceGroupName --query name -o tsv
+$inputs = @{
+  "WhatIf" = $WhatIf
+}
+
+Write-Hash "Inputs" $inputs
+
+$sharedResourceGroupName = "shared"
+$sharedRgString = 'klgoyi'
+$resourceGroupLocation = "westus3"
+$personalProjectsResourceGroupName = "personalprojects"
 
 Write-Step "Fetch params from Azure"
 $sharedResourceVars = Get-SharedResourceDeploymentVars $sharedResourceGroupName $sharedRgString
@@ -36,14 +41,31 @@ $data = [ordered]@{
 
 Write-Hash "Data" $data
 
-Write-Step "Build and Push $clientImageName Image"
-docker build -t $clientImageName ./site
-docker push $clientImageName
-# az acr build -r $registryUrl -t $clientImageName ./site
+Write-Step "Create Resource Group $personalProjectsResourceGroupName"
+az group create -l $resourceGroupLocation -g $personalProjectsResourceGroupName --query name -o tsv
 
-Write-Step "Deploy $clientImageName Container App"
+Write-Step "Provision $personalProjectsResourceGroupName Resources (What-If: $($WhatIf))"
+
+Write-Step "Build $clientImageName Image (What-If: $($WhatIf))"
+docker build -t $clientImageName "$repoRoot/site"
+
+if ($WhatIf -eq $False) {
+  Write-Step "Push $clientImageName Image (What-If: $($WhatIf))"
+  docker push $clientImageName
+  # az acr build -r $registryUrl -t $clientImageName ./site
+}
+else {
+  Write-Step "Skipping Push $clientImageName Image (What-If: $($WhatIf))"
+}
+
+Write-Step "Get Top Image from $($sharedResourceVars.registryUrl) respository $clientContainerName to Verify Push (What-If: $($WhatIf))"
+az acr repository show-tags --name $($sharedResourceVars.registryUrl)  --repository $clientContainerName --orderby time_desc --top 1 -o tsv
+
+Write-Step "Deploy $clientImageName Container App (What-If: $($WhatIf))"
 $clientBicepContainerDeploymentFilePath = "$repoRoot/bicep/modules/clientContainerApp.bicep"
-$clientFqdn = $(az deployment group create `
+
+if ($WhatIf -eq $True) {
+  az deployment group create `
     -g $personalProjectsResourceGroupName `
     -f $clientBicepContainerDeploymentFilePath `
     -p managedEnvironmentResourceId=$($sharedResourceVars.containerAppsEnvResourceId) `
@@ -52,8 +74,21 @@ $clientFqdn = $(az deployment group create `
     registryPassword=$($sharedResourceVars.registryPassword) `
     imageName=$clientImageName `
     containerName=$clientContainerName `
-    --query "properties.outputs.fqdn.value" `
-    -o tsv)
-
-$clientUrl = "https://$clientFqdn"
-Write-Output $clientUrl
+    --what-if
+}
+else {
+  $clientFqdn = $(az deployment group create `
+      -g $personalProjectsResourceGroupName `
+      -f $clientBicepContainerDeploymentFilePath `
+      -p managedEnvironmentResourceId=$($sharedResourceVars.containerAppsEnvResourceId) `
+      registryUrl=$($sharedResourceVars.registryUrl) `
+      registryUsername=$($sharedResourceVars.registryUsername) `
+      registryPassword=$($sharedResourceVars.registryPassword) `
+      imageName=$clientImageName `
+      containerName=$clientContainerName `
+      --query "properties.outputs.fqdn.value" `
+      -o tsv)
+  
+  $clientUrl = "https://$clientFqdn"
+  Write-Output $clientUrl
+}
